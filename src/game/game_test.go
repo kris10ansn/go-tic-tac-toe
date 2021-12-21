a3cf6e5fbf7fa9ec493acc64dbb2f1c09680a2db
BuildTools <unconfigured@null.spigotmc.org>
2021-12-22 00:48:24 +0100

    add test file with (unfinished) CheckWin tests

---
+++ src/game/game_test.go
@@ -0,0 +1,41 @@
+package game
+
+import (
+	"testing"
+)
+
+type Row = [BOARD_WIDTH]Tic
+
+var tics = [2]Tic{X_TIC, O_TIC}
+var empty_row = Row{EMPTY_TIC, EMPTY_TIC, EMPTY_TIC}
+var horizontal_x_row = Row{X_TIC, X_TIC, X_TIC}
+var horizontal_o_row = Row{O_TIC, O_TIC, O_TIC}
+
+func TestTableCheckWin(t *testing.T) {
+	var tests = []struct {
+		input    Board
+		expected Tic
+	}{
+		// Horizontal x boards
+		{Board{horizontal_x_row, empty_row, empty_row}, X_TIC},
+		{Board{empty_row, horizontal_x_row, empty_row}, X_TIC},
+		{Board{empty_row, empty_row, horizontal_x_row}, X_TIC},
+		// Horizontal o boards
+		{Board{horizontal_o_row, empty_row, empty_row}, O_TIC},
+		{Board{empty_row, horizontal_o_row, empty_row}, O_TIC},
+		{Board{empty_row, empty_row, horizontal_o_row}, O_TIC},
+
+		// TODO: vertical tests, diagonal tests
+	}
+
+	for _, test := range tests {
+		if output := CheckWin(test.input); output != test.expected {
+			t.Errorf(
+				"\nTest Failed: \n%s inputted, \n%c expected, \n%c recieved",
+				BoardToString(test.input),
+				test.expected,
+				output,
+			)
+		}
+	}
+}
