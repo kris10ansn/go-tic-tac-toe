56a7a6444d334b9494ed904630aaee763f2ac6ee
BuildTools <unconfigured@null.spigotmc.org>
2021-12-22 00:05:03 +0100

    check for winner after each move, print win/draw/error message

--- src/main.go
+++ src/main.go
@@ -8,25 +8,30 @@ import (
 )
 
 func main() {
+	var winner game.Tic = game.EMPTY_TIC
 	var turn game.Tic = game.X_TIC
 	var moves byte = 0
 
 	gameBoard := game.CreateEmptyBoard()
 	cli.PrintBoard(gameBoard)
 
-	for ; moves < 9; moves++ {
+	for ; winner == game.EMPTY_TIC && moves < 9; moves++ {
 		fmt.Printf("%c's turn\n", turn)
 
 		x, y := cli.WaitForMove(gameBoard)
 
 		gameBoard[y][x] = turn
-
-		// TODO: check win
+		winner = game.CheckWin(gameBoard)
 
 		cli.PrintBoard(gameBoard)
-
 		game.NextTurn(&turn)
 	}
 
-	// TODO: draw
+	if winner != game.EMPTY_TIC {
+		fmt.Printf("%c won the game!\n", winner)
+	} else if moves == 9 {
+		fmt.Println("It's a draw!")
+	} else {
+		panic("Unexpected behaviour")
+	}
 }
