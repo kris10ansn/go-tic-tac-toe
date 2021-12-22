4c8680b177c342eacb5233ed80ce4bedbe73e200
BuildTools <unconfigured@null.spigotmc.org>
2021-12-22 01:00:49 +0100

    add vertical and diagonal tests

--- src/game/game_test.go
+++ src/game/game_test.go
@@ -11,11 +11,13 @@ var empty_row = Row{EMPTY_TIC, EMPTY_TIC, EMPTY_TIC}
 var horizontal_x_row = Row{X_TIC, X_TIC, X_TIC}
 var horizontal_o_row = Row{O_TIC, O_TIC, O_TIC}
 
+type CheckWinTest struct {
+	input    Board
+	expected Tic
+}
+
 func TestTableCheckWin(t *testing.T) {
-	var tests = []struct {
-		input    Board
-		expected Tic
-	}{
+	var tests = []CheckWinTest{
 		// Horizontal x boards
 		{Board{horizontal_x_row, empty_row, empty_row}, X_TIC},
 		{Board{empty_row, horizontal_x_row, empty_row}, X_TIC},
@@ -25,7 +27,23 @@ func TestTableCheckWin(t *testing.T) {
 		{Board{empty_row, horizontal_o_row, empty_row}, O_TIC},
 		{Board{empty_row, empty_row, horizontal_o_row}, O_TIC},
 
-		// TODO: vertical tests, diagonal tests, generated tests?
+		// Vertical x boards
+		{Board{{X_TIC, EMPTY_TIC, EMPTY_TIC}, {X_TIC, EMPTY_TIC, EMPTY_TIC}, {X_TIC, EMPTY_TIC, EMPTY_TIC}}, X_TIC},
+		{Board{{EMPTY_TIC, X_TIC, EMPTY_TIC}, {EMPTY_TIC, X_TIC, EMPTY_TIC}, {EMPTY_TIC, X_TIC, EMPTY_TIC}}, X_TIC},
+		{Board{{EMPTY_TIC, EMPTY_TIC, X_TIC}, {EMPTY_TIC, EMPTY_TIC, X_TIC}, {EMPTY_TIC, EMPTY_TIC, X_TIC}}, X_TIC},
+
+		// Vertical o boards
+		{Board{{O_TIC, EMPTY_TIC, EMPTY_TIC}, {O_TIC, EMPTY_TIC, EMPTY_TIC}, {O_TIC, EMPTY_TIC, EMPTY_TIC}}, O_TIC},
+		{Board{{EMPTY_TIC, O_TIC, EMPTY_TIC}, {EMPTY_TIC, O_TIC, EMPTY_TIC}, {EMPTY_TIC, O_TIC, EMPTY_TIC}}, O_TIC},
+		{Board{{EMPTY_TIC, EMPTY_TIC, O_TIC}, {EMPTY_TIC, EMPTY_TIC, O_TIC}, {EMPTY_TIC, EMPTY_TIC, O_TIC}}, O_TIC},
+
+		// Diagonal x boards
+		{Board{{X_TIC, EMPTY_TIC, EMPTY_TIC}, {EMPTY_TIC, X_TIC, EMPTY_TIC}, {EMPTY_TIC, EMPTY_TIC, X_TIC}}, X_TIC},
+		{Board{{EMPTY_TIC, EMPTY_TIC, X_TIC}, {EMPTY_TIC, X_TIC, EMPTY_TIC}, {X_TIC, EMPTY_TIC, EMPTY_TIC}}, X_TIC},
+
+		// Diagonal o boards
+		{Board{{O_TIC, EMPTY_TIC, EMPTY_TIC}, {EMPTY_TIC, O_TIC, EMPTY_TIC}, {EMPTY_TIC, EMPTY_TIC, O_TIC}}, O_TIC},
+		{Board{{EMPTY_TIC, EMPTY_TIC, O_TIC}, {EMPTY_TIC, O_TIC, EMPTY_TIC}, {O_TIC, EMPTY_TIC, EMPTY_TIC}}, O_TIC},
 	}
 
 	for _, test := range tests {
