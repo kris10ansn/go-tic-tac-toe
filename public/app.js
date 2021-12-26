1b7fa75c9f7fb681f7c0ad1e10432173c2b5720e
Kristian Silli Nessa <kristian.nessa@protonmail.com>
2021-12-26 23:45:12 +0100

    add more listeners and log statements, format with prettier

--- public/app.js
+++ public/app.js
@@ -1,14 +1,26 @@
 function createElement(htmlString) {
-    const element = new DOMParser().parseFromString(string)
-    return element.documentElement.querySelector("body").firstChild
+    const element = new DOMParser().parseFromString(string);
+    return element.documentElement.querySelector("body").firstChild;
 }
 
 function main() {
-    const gamesSocket = new WebSocket(`ws:/${location.host}/socket/games`)
+    const gamesSocket = new WebSocket(`ws:/${location.host}/socket/games`);
 
     gamesSocket.addEventListener("open", () => {
-        console.log("Websocket connected!")
-    }) 
+        console.log("Websocket connected!");
+
+        gamesSocket.addEventListener("message", (message) => {
+            console.log("Websocket message", message);
+        });
+
+        gamesSocket.addEventListener("close", (event) => {
+            console.log("Websocket connection closed", event);
+        });
+
+        gamesSocket.addEventListener("error", (error) => {
+            console.error("Websocket error", error);
+        });
+    });
 }
 
-main()
+main();
