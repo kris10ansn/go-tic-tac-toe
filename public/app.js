f835405ff6772adc9084ec90aea6c99dfa9131b3
Kristian Silli Nessa <kristian.nessa@protonmail.com>
2021-12-27 14:47:54 +0100

    update website functionality

--- public/app.js
+++ public/app.js
@@ -1,16 +1,57 @@
 function createElement(htmlString) {
-    const element = new DOMParser().parseFromString(string);
+    const element = new DOMParser().parseFromString(htmlString, "text/html");
     return element.documentElement.querySelector("body").firstChild;
 }
 
-function main() {
+function appendGameToList({ id, name }) {
+    const gamesList = document.querySelector("ul#games");
+    gamesList.appendChild(
+        createElement(`
+            <li id="${id}">
+                ${name} <button>join game</button>
+            </li>
+        `)
+    );
+}
+
+function handleCreateGameSubmits() {
+    const createGameForm = document.forms.namedItem("create-game");
+
+    createGameForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const data = Object.fromEntries(new FormData(createGameForm).entries());
+
+        fetch(`http://${location.host}/game/create`, {
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+
+        createGameForm.reset();
+    });
+}
+
+function connectGamesSocket() {
     const gamesSocket = new WebSocket(`ws:/${location.host}/socket/games`);
 
     gamesSocket.addEventListener("open", () => {
         console.log("Websocket connected!");
 
-        gamesSocket.addEventListener("message", (message) => {
-            console.log("Websocket message", message);
+        gamesSocket.addEventListener("message", (msg) => {
+            const message = JSON.parse(msg.data);
+
+            switch (message.type) {
+                case "add-game": {
+                    appendGameToList(message.data);
+                    break;
+                }
+                default: {
+                    console.log(
+                        "Websocket message with unknown message type received",
+                        message
+                    );
+                }
+            }
         });
 
         gamesSocket.addEventListener("close", (event) => {
@@ -21,6 +62,17 @@ function main() {
             console.error("Websocket error", error);
         });
     });
+}
+
+async function main() {
+    const games = await fetch(`http://${location.host}/game/list`).then(
+        (response) => response.json()
+    );
+
+    games.forEach((game) => appendGameToList(game));
+
+    connectGamesSocket();
+    handleCreateGameSubmits();
 }
 
 main();
