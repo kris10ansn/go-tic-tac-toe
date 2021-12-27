d46a0cace9416feb62b1ed6e55eebe4eb18f0490
Kristian Silli Nessa <kristian.nessa@protonmail.com>
2021-12-27 15:38:25 +0100

    temporarily remove web import

--- src/main.go
+++ src/main.go
@@ -6,16 +6,12 @@ import (
 	"github.com/kris10ansn/go-tic-tac-toe/src/cli"
 	"github.com/kris10ansn/go-tic-tac-toe/src/game"
 	"github.com/kris10ansn/go-tic-tac-toe/src/game/frontend/commandline"
-	"github.com/kris10ansn/go-tic-tac-toe/src/game/frontend/web"
 )
 
 func main() {
 	switch mode := cli.GetArgument(1, "cli"); mode {
 	case "cli":
 		game.PlayGame(commandline.New())
-	case "web":
-		gameServer := web.CreateServer()
-		gameServer.Run()
 	default:
 		panic(fmt.Sprintf("Unsupported mode: \"%s\" (arg 1)", mode))
 	}
