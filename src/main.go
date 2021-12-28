b9f4382fe0f47451c61606afa2c0fb57f9b19aef
Kristian Silli Nessa <kristian.nessa@protonmail.com>
2021-12-29 00:47:55 +0100

    add web command line option

--- src/main.go
+++ src/main.go
@@ -6,12 +6,16 @@ import (
 	"github.com/kris10ansn/go-tic-tac-toe/src/cli"
 	"github.com/kris10ansn/go-tic-tac-toe/src/game"
 	"github.com/kris10ansn/go-tic-tac-toe/src/game/frontend/commandline"
+	"github.com/kris10ansn/go-tic-tac-toe/src/game/frontend/web"
 )
 
 func main() {
 	switch mode := cli.GetArgument(1, "cli"); mode {
 	case "cli":
 		game.PlayGame(commandline.New())
+	case "web":
+		server := web.CreateServer()
+		server.Run()
 	default:
 		panic(fmt.Sprintf("Unsupported mode: \"%s\" (arg 1)", mode))
 	}
