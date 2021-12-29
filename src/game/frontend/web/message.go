9ba1d1d2043c83e3f058591e7ed70684a9ba1ac1
Kristian Silli Nessa <kristian.nessa@protonmail.com>
2021-12-29 02:13:07 +0100

    add message-type constants

--- src/game/frontend/web/message.go
+++ src/game/frontend/web/message.go
@@ -1,5 +1,13 @@
 package web
 
+const (
+	MessageTypePresentBoard = "present-board"
+	MessageTypeError        = "error"
+	MessageTypeAddGame      = "add-game"
+	MessageTypeAssignTic    = "assign-tic"
+	MessageTypeEndGame      = "end-game"
+)
+
 type WebsocketMessage struct {
 	Type string      `json:"type"`
 	Data interface{} `json:"data"`
